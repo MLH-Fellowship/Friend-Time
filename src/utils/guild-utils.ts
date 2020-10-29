4c9d86df814a5ed96007d1a669f05ebc1c8b5289
NoahCardoza <noahcardoza@gmail.com>
2020-10-29 15:41:15 -0700

    respect RESPOND_TO_BOT_MESSAGES and RESPOND_TO_BOT_REACTIONS env

--- src/utils/guild-utils.ts
+++ src/utils/guild-utils.ts
@@ -2,7 +2,11 @@ import { Guild, GuildMember } from 'discord.js';
 
 export abstract class GuildUtils {
     public static getMemberDiscordIds(guild: Guild): string[] {
-        return guild.members.cache.filter(member => !member.user.bot).keyArray();
+        if ((process.env.RESPOND_TO_BOT_MESSAGES || process.env.RESPOND_TO_BOT_REACTIONS)  === 'true') {
+            return guild.members.cache.keyArray();
+        } else {
+            return guild.members.cache.filter(member => !member.user.bot).keyArray();
+        }
     }
 
     public static async findMember(guild: Guild, query: string): Promise<GuildMember> {
